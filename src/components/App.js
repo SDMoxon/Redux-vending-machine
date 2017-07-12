07f8bc25c16defd29427ff566b20ff1e52793f3e
Steven <steven_moxon123@hotmail.co.uk>
2017-07-12 15:41:54 +0100

    [App Component] import state from react-redux store

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,44 @@
 import React from 'react';
-
+import {connect} from 'react-redux';
 // Components 
 import InputArea from './InputArea';
 import ProductArea from './ProductArea';
+import PropTypes from 'prop-types';
 
 class App extends React.Component {
-    render () {
+    render() {
         return (
             <div className="container">
                 <h1>Vending Machine</h1>
-                <InputArea/>
-                <ProductArea/>
+                <InputArea />
+                <ProductArea />
             </div>
         );
     }
 }
 
-export default App;
+function mapStateToProps(state) {
+    return {
+        products: state.stock,
+        inputs: {
+            credit: state.credit,
+            changeArea: state.changeArea,
+            float: state.float,
+            displayMessage: state.displayMessage,
+            selection: state.selection
+        },
+        productDispenser: {
+            productDispenser: state.productDispenser,
+            dispenserDoor: state.dispenserDoorOpen
+        }
+    };
+}
+
+
+export default connect( mapStateToProps)(App);
+
+App.propTypes = {
+    products: PropTypes.object.isRequired,
+    inputs: PropTypes.object.isRequired,
+    productDispenser: PropTypes.object.isRequired
+};
--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import {render} from 'react-dom';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import {reducer} from '../reducers/reducer';
+
+const store = createStore(reducer);
 
 // Components
 import App from './components/App';
 
 render (
-    <App/>, document.querySelector('#root')
+    <Provider store={store}>
+        <App/>
+    </Provider>    
+        , document.querySelector('#root')
 );
